364a7ca825337a47332290bffb45473d1390ed3f
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-24 04:06:37 +0300

    chore: Update static file configuration in settings.py

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
+        engine='django.db.backends.postgresql',
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
@@ -190,8 +190,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
@@ -201,7 +201,7 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
